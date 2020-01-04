d21dd119669ef4fad964567ddf719803e73b37fd
M.Yasini <yasinimahdi98@gmail.com>
2020-01-04 22:36:09 +0330

    changes

--- src/thirdSlide/thirdSlide.js
+++ src/thirdSlide/thirdSlide.js
@@ -1,41 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style/thirdSlide.css';
-import Suitcase from './images/portfolio.png'
-import Heart from './images/heart.png'
-import Clock from './images/clock.png'
-import Star from './images/star.png'
-const mainSlide = () => {
-    return (
-        <div >
-            <div id="thirdSlide">
-                <div className="thirdSlideTopDiv">
-                    <div className="thirdSlideTopWorkState">
-                        <div className="workState">
-                            <img className="workIcons" src={Suitcase} alt="Suitcase"></img>
-                            <p className="workPassage">548<br />PROJECTS COMPLETED</p>
+import Suitcase from './images/portfolio.png';
+import Heart from './images/heart.png';
+import Clock from './images/clock.png';
+import Star from './images/star.png';
+import TeslaModelX from './images/tesla.jpg';
+import AudiA8 from './images/a8.jpg';
+import ChevroletCamaro from './images/camaro.jpg';
+import BenzGClass from './images/gClass.jpg';
+import VWPassat from './images/passat.jpg';
+import BenzSClass from './images/s.jpg';
+import BmwX1 from './images/x1.jpg';
+import VolvoXC90 from './images/xc90.jpg';
+import Eye from './images/eye.png'
+
+class mainSlide extends Component {
+    state = {
+        products: [
+            { imageSrc: TeslaModelX, alt: 'TeslaModelX' },
+            { imageSrc: AudiA8, alt: 'AudiA8' },
+            { imageSrc: ChevroletCamaro, alt: 'ChevroletCamaro' },
+            { imageSrc: BenzGClass, alt: 'BenzGClass' },
+            { imageSrc: VWPassat, alt: 'VWPassat' },
+            { imageSrc: BenzSClass, alt: 'BenzSClass' },
+            { imageSrc: BmwX1, alt: 'BmwX1' },
+            { imageSrc: VolvoXC90, alt: 'VolvoXC90' },
+        ],
+    }
+    render() {
+
+        let products = (
+            <div className="middleThirdSlideProductsState">
+                {this.state.products.map((product) => {
+                    return <div className="carImageDiv">
+                        <img src={product.imageSrc} alt={product.alt}></img>
+                        <div class="overlay">
+                            {/* <h2>Check This <span>Out!</span></h2> */}
+                            <a href="#" class="icon" title="User Profile">
+                                <i class="fa fa-user fa-3x"></i>
+                                <img src= {Eye} alt="eye"></img>
+                            </a>
                         </div>
                     </div>
-                    <div className="thirdSlideTopWorkState">
-                        <div className="workState">
-                            <img className="workIcons" src={Clock} alt="Clock"></img>
-                            <p className="workPassage">1465<br />WORK HOURS</p>
+                })}
+            </div>
+        );
+        return (
+            <div >
+                <div id="thirdSlide">
+                    <div className="thirdSlideTopDiv">
+                        <div className="thirdSlideTopWorkState">
+                            <div className="workState">
+                                <img className="workIcons" src={Suitcase} alt="Suitcase"></img>
+                                <p className="workPassage">548<br />PROJECTS COMPLETED</p>
+                            </div>
+                        </div>
+                        <div className="thirdSlideTopWorkState">
+                            <div className="workState">
+                                <img className="workIcons" src={Clock} alt="Clock"></img>
+                                <p className="workPassage">1465<br />WORK HOURS</p>
+                            </div>
+                        </div>
+                        <div className="thirdSlideTopWorkState">
+                            <div className="workState">
+                                <img className="workIcons" src={Star} alt="Star"></img>
+                                <p className="workPassage">612<br />POSITIVE FEEDBACKS</p>
+                            </div>
+                        </div>
+                        <div className="thirdSlideTopWorkState">
+                            <div className="workState">
+                                <img className="workIcons" src={Heart} alt="Heart"></img>
+                                <p className="workPassage">735<br />HAPPY CLIENTS</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="thirdSlideTopWorkState">
-                        <div className="workState">
-                            <img className="workIcons" src={Star} alt="Star"></img>
-                            <p className="workPassage">612<br />POSITIVE FEEDBACKS</p>
-                        </div>
-                    </div>
-                    <div className="thirdSlideTopWorkState">
-                        <div className="workState">
-                            <img className="workIcons" src={Heart} alt="Heart"></img>
-                            <p className="workPassage">735<br />HAPPY CLIENTS</p>
-                        </div>
+                    {products}
+                    <div className="buttonThirdSlideState">
+                        <p className="buttonThirdSlidePassage">LOAD MORE WORK</p>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 export default mainSlide;
